6c0d5253c85d3e7492bae456ca5b8ead970c97d1
Matthew Wilhelm <matthew.wilhelm@uconn.edu>
2022-01-05 11:08:31 -0500

    Update example

--- example/pratt_whitney_127/pratt_whitney_127.jl
+++ example/pratt_whitney_127/pratt_whitney_127.jl
@@ -1,27 +1,16 @@
-using Pkg
+using Pkg, CPLEX
 #=
 Pkg.add(url="PATH_TO_JOBSHOP_PACKAGE HERE")
 # or
 =#
 #Pkg.rm("JobShop")
-Pkg.develop(path="C:\\Users\\wilhe\\OneDrive\\Desktop\\Package Develop\\Jobshop.jl")
+Pkg.develop(path="C:\\Users\\wilhe\\Dropbox\\My PC (DESKTOP-P6322LG)\\Desktop\\New Job Shop\\Uploaded Jobshop\\Jobshop.jl")
 
 using JobShop
 
 jsprob = load_from_csv(@__DIR__)
 jsprob.parameter.start_estimate = 1400.0
-for i in 1:5
-    jsprob.I[i] = Int[i]
-end
-for (k,i) in enumerate(6:10:106)
-    jsprob.I[k + 5] = Int[j for j=i:(i+9)]
-end
-for (k,i) in enumerate(117:127)
-    jsprob.I[k + 16] = Int[i]
-end
-for i = 1:127
-    jsprob.w[i] = 1
-end
+jsprob.parameter.optimizer = CPLEX.Optimizer
 
 
 sequential_solve!(jsprob)
