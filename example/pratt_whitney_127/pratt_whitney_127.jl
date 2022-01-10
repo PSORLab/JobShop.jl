b0854f9fc91c6df455726f7241ec97ad7cef096e
Matthew Wilhelm <matthew.wilhelm@uconn.edu>
2022-01-10 14:09:15 -0500

    Add hyperparameter specifications to problems

--- example/pratt_whitney_127/pratt_whitney_127.jl
+++ example/pratt_whitney_127/pratt_whitney_127.jl
@@ -13,7 +13,16 @@ jsprob.parameter.start_upper_bound = 8000.0
 jsprob.parameter.ShiftLength = 18
 jsprob.parameter.prob = 0.05
 jsprob.parameter.prob_r = 0.2
+jsprob.T = 1:220
+jsprob.parameter.start_norm = 100.0
+jsprob.parameter.start_step = 0.006
+jsprob.parameter.penalty = 120.0
 jsprob.parameter.optimizer = CPLEX.Optimizer
-
+jsprob.parameter.alpha_step_1 = 0.5/20
+jsprob.parameter.alpha_step_2 = 0.5
+jsprob.parameter.feasible_interval = 7
+jsprob.parameter.feasible_norm_limit = 3.0
+jsprob.parameter.stepsize_interval = 25
+jsprob.parameter.stepsize_start = 37.5
 
 sequential_solve!(jsprob)
--- example/small_example/sample_example.jl
+++ example/small_example/sample_example.jl
@@ -9,7 +9,16 @@ jsprob.parameter.start_upper_bound = 8000.0
 jsprob.parameter.ShiftLength = 18
 jsprob.parameter.prob = 0.05
 jsprob.parameter.prob_r = 0.2
+jsprob.T = 1:75
+jsprob.parameter.start_norm = 100.0
+jsprob.parameter.start_step = 0.006
 jsprob.parameter.optimizer = CPLEX.Optimizer
-
+jsprob.parameter.penalty = 60.0
+jsprob.parameter.alpha_step_1 = 0.5/20
+jsprob.parameter.alpha_step_2 = 0.5
+jsprob.parameter.feasible_interval = 21
+jsprob.parameter.feasible_norm_limit = 2.0
+jsprob.parameter.stepsize_interval = 40
+jsprob.parameter.stepsize_start = 37.5
 
 sequential_solve!(jsprob)
