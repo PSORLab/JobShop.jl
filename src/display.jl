81c7e5d4b8ce179b55e43ea186f620b83dfaf661
Matthew Wilhelm <matthew.wilhelm@uconn.edu>
2022-01-11 12:14:56 -0500

    Add qual calculation

--- src/display.jl
+++ src/display.jl
@@ -7,10 +7,11 @@ function display_iteration(j::JobShopProblem, i::Int)
     @unpack maxest, estimate, penalty, current_iteration, time_start, time_solve_subprob, 
             time_solve_stepsize, current_norm, current_step = j.status
     total_time = time() - time_start
+    qual = current_abs_gap(j)/estimate*100
     if (current_upper_bound(j) < 100000.0) || (j.parameter.verbosity > 1)
         outstring = @sprintf("Iteration:  %4i, (S):  %4i  ", current_iteration, i)
         outstring *= @sprintf("SDV:  %.3e  FC:  %.3e  ", current_lower_bound(j), current_upper_bound(j))
-        outstring *= @sprintf("GAP:  %.3e  Qual.:  %.3e  ", current_abs_gap(j), current_rel_gap(j))
+        outstring *= @sprintf("GAP:  %.3e  Qual.:  %.3e  ", current_abs_gap(j), qual)
         outstring *= @sprintf("Total Time: %.1f  %.1f  %.1f ", total_time, time_solve_subprob, time_solve_stepsize)
         outstring *= @sprintf("Norm:  %.3e  %.3e  %.3e  %.3e  %.3e	", current_norm, maxest, estimate, current_step, penalty)
         println(outstring)
