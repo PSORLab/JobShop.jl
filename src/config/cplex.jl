ce313a2d5ad3deed494906514cce65b059c5a67a
Matthew Wilhelm <matthew.wilhelm@uconn.edu>
2022-01-11 15:18:07 -0500

    Updated CPLEX config

--- src/config/cplex.jl
+++ src/config/cplex.jl
@@ -2,11 +2,14 @@
 
 =#
 
+const CPLEX_SEED = 12
+
 function configure!(::Subproblem, v::Val{:CPLEX}, j::JobShopProblem, m::Model)
+    set_optimizer_attribute(m, "CPX_PARAM_RANDOMSEED", CPLEX_SEED)
     set_optimizer_attribute(m, "CPX_PARAM_REPAIRTRIES", 200000)
     set_optimizer_attribute(m, "CPX_PARAM_RINSHEUR",    200000)
     set_optimizer_attribute(m, "CPX_PARAM_HEURFREQ",    200000)
-    if current_iteration(j) >= 1
+    if current_iteration(j) == 1
         set_time_limit_sec(m, 60)
         set_optimizer_attribute(m, "CPX_PARAM_EPGAP", 0.01)
     end
@@ -21,7 +24,9 @@ function configure!(::Subproblem, v::Val{:CPLEX}, j::JobShopProblem, m::Model)
 end
 
 function configure!(::FeasibilityProblem, v::Val{:CPLEX}, j::JobShopProblem, m::Model)
+    set_time_limit_sec(m, 180)
     set_optimizer_attribute(m, "CPX_PARAM_CUTUP", current_upper_bound(j))
+    set_optimizer_attribute(m, "CPX_PARAM_RANDOMSEED", CPLEX_SEED)
     set_optimizer_attribute(m, "CPX_PARAM_REPAIRTRIES", 1000000)
     set_optimizer_attribute(m, "CPX_PARAM_RINSHEUR",    1000000)
     set_optimizer_attribute(m, "CPX_PARAM_HEURFREQ",    1000000)
@@ -31,5 +36,6 @@ end
 
 function configure!(::StepsizeProblem, v::Val{:CPLEX}, j::JobShopProblem, m::Model)
     set_optimizer_attribute(m, "CPX_PARAM_CUTUP", 0.0)
+    set_optimizer_attribute(m, "CPX_PARAM_RANDOMSEED", CPLEX_SEED)
     return
 end
