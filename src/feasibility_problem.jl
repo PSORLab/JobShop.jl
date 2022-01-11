bf460b5f2a2d787d3db79b956a5bd2d046aa6359
Matthew Wilhelm <matthew.wilhelm@uconn.edu>
2022-01-10 20:16:01 -0500

    Update feasibility problem parameters

--- src/feasibility_problem.jl
+++ src/feasibility_problem.jl
@@ -10,7 +10,7 @@ function solve_problem(::FeasibilityProblem, jsp::JobShopProblem)
 
     @unpack I, J, Jop, PartDue, MachineCap, MachineType, R, T, IJT, MIJ, sbTime1 = jsp
     @unpack upper_bound = jsp.status
-    @unpack prob, prob_r, ShiftLength = jsp.parameter 
+    @unpack prob, prob_r, ShiftLength, feasibility_window = jsp.parameter 
 
     m = direct_model(optimizer_with_attributes(jsp.parameter.optimizer))
     configure!(FeasibilityProblem(), jsp, m)
@@ -19,13 +19,17 @@ function solve_problem(::FeasibilityProblem, jsp::JobShopProblem)
     @variables(m, begin          
         0 <= cTime1[i=I, Jop[i]] <= 1000, Int
         0 <= cTime2[i=I, Jop[i], Jop[i], R] <= 1000, Int 
+        bTimeI1[i=I, Jop[i], T], Bin
+
         0 <= bTime1[i=I, Jop[i]] <= 1000, Int                   
         0 <= bTime2[i=I, Jop[i], Jop[i], R] <= 1000, Int
-        bTimeI1[i=I, Jop[i], T], Bin
-        bTimeI2[i=I, Jop[i], Jop[i], R, T], Bin 
+        bTimeI2[i=I, Jop[i], Jop[i], R, T], Bin
+
         0 <= ComTime1[I] <= 1000, Int
-        0 <= ComTime2[i=I,Jop[i],R] <= 1000, Int   
+        0 <= ComTime2[i=I,Jop[i],R] <= 1000, Int
+
         0 <= y[i=I, Jop[i]] <= 1000, Int   
+
         # variables used for first SOS rearrangement
         0 <= W11[I] <= 1
         0 <= W21[I] <= 1
@@ -33,6 +37,7 @@ function solve_problem(::FeasibilityProblem, jsp::JobShopProblem)
         alpha11[I], Bin  
         alpha21[I], Bin 
         alpha31[I], Bin
+
         # variables used for second SOS rearrangement
         0 <= W12[i=I, j=Jop[i], R] <= 1
         0 <= W22[i=I, j=Jop[i], R] <= 1
@@ -55,54 +60,65 @@ function solve_problem(::FeasibilityProblem, jsp::JobShopProblem)
     @objective(m, Min, TotalTard)
 
     @constraints(m, begin
+
+        [i=I, j=Jop[i]],        cTime1[i,j] + 1 <= bTime2[i,1,j,1]
+        [i=I, j=Jop[i]],        cTime1[i,j] + 1 <= bTime2[i,j,j,2]
+
         W11 .<= alpha11 
         W21 .<= alpha21
         W31 .<= alpha31
         W11 .+ W21 .+ W31 .== 1 
         alpha11 .+ alpha31 .<= 1 
         alpha21 .== 1
-        [i=I],                  ComTime1[i] - cTime1[i,J[i]] == 0.0
-        [i=I, j1=Jop[i], r=R],  ComTime2[i,j1,r] == cTime2[i,J[i],j1,r]
-        [i=I, j=Jop[i]],        cTime1[i,j] + 1 <= bTime2[i,1,j,1]
-        [i=I, j=Jop[i]],        cTime1[i,j] + 1 <= bTime2[i,j,j,2]
+        [i=I],  ComTime1[i] - PartDue[i] == W11[i]*(-T[end]) + W31[i]*2*T[end]
+
         [i=I, j=Jop[i], r=R],   W12[i,j,r] <= alpha12[i,j,r]
         [i=I, j=Jop[i], r=R],   W22[i,j,r] <= alpha22[i,j,r]
         [i=I, j=Jop[i], r=R],   W32[i,j,r] <= alpha32[i,j,r]
         [i=I, j=Jop[i], r=R],   W12[i,j,r] + W22[i,j,r] + W32[i,j,r] == 1
         [i=I, j=Jop[i], r=R],   alpha12[i,j,r] + alpha32[i,j,r] <= 1
         [i=I, j=Jop[i], r=R],   alpha22[i,j,r] == 1
-        [i=I, j=1:(J[i]-1)],    cTime1[i,j] + 1 <= bTime1[i,j+1]
-        [i=I, j=Jop[i]],        sum(bTimeI1[i,j,t] for t in T, P in IJT if has_ij(P,i,j)) == 1
-        [i=I, j=Jop[i]],        sum(t*bTimeI1[i,j,t] for t in T, P in IJT if has_ij(P,i,j)) == bTime1[i,j]
-        [i=I, j=Jop[i]],        sum((t+P.t)*bTimeI1[i,j,t] for t in T, P in IJT if has_ij(P,i,j)) - 1 == cTime1[i,j]
+        [i=I, j=Jop[i], r=R],   ComTime2[i,j,r] - PartDue[i] == W12[i,j,r]*(-T[end]) + W32[i,j,r]*2*T[end]
+
+        [i=I, j=1:(J[i]-1)],                    cTime1[i,j] + 1  <= bTime1[i,j+1]
+        [i=I, j1=Jop[i], r=R, j = 1:(J[i]-1)],  cTime2[i,j,j1,r] <= bTime2[i,j+1,j1,r] - 1  # TODO: maybe...
+
         [i=I, j1=Jop[i]; J[i] >= 2],    y[i,j1] >= cTime1[i,j1]/ShiftLength
         [i=I, j1=Jop[i]; J[i] >= 2],    y[i,j1] <= cTime1[i,j1]/ShiftLength+0.9999
         [i=I, j1=Jop[i]; J[i] >= 2],    y[i,j1] <= (bTime2[i,1,j1,1]-1)/ShiftLength
         [i=I, j1=Jop[i]; J[i] >= 2],    y[i,j1] <= (bTime2[i,j1,j1,2]-1)/ShiftLength
-        [i=I, j1=Jop[i], r=R, j = 1:(J[i]-1)],  cTime2[i,j,j1,r] <= bTime2[i,j+1,j1,r] - 1
-        [i=I, j=Jop[i]],    bTime1[i,j] - sbTime1[i,j] <= 1
-        [i=I, j=Jop[i]],    bTime1[i,j] - sbTime1[i,j] >= -1
-    end)
-
-    @constraints(m, begin
-        [i=I, j=Jop[i], j1=Jop[i], r=R], sum(bTimeI2[i,j,j1,r,t] for t in T, P in IJT if has_ij(P,i,j)) == 1
-        [i=I, j=Jop[i], j1=Jop[i], r=R], sum(t*bTimeI2[i,j,j1,r,t] for t in T, P in IJT if has_ij(P,i,j)) == bTime2[i,j,j1,r]
-        [i=I, j=Jop[i], j1=Jop[i], r=R], sum((t+P.t)*bTimeI2[i,j,j1,r,t] for t in T, P in IJT if has_ij(P,i,j)) - 1  == cTime2[i,j,j1,r]
     end)
 
     @constraint(m, [mi in MachineType, t in T],
         sum(((1-prob)^(Ma.j-1))*sum(bTimeI1[Ma.i,Ma.j,k] for k in (t - p.t+1):t if (t - p.t+1 >= 1)) for p in IJT, Ma in MIJ if (p.i == Ma.i && p.j == Ma.j && Ma.m == mi)) +
         sum(((1-prob)^(j-1)-(1-prob)^(j))*(1-prob_r)*sum(bTimeI2[Ma.i,Ma.j,j,1,k] for k in (t - p.t+1):t if (t - p.t+1 >= 1)) for Ma in MIJ, p in IJT, j in Jop[Ma.i] if (p.i == Ma.i && p.j == Ma.j && Ma.m == mi)) +
         sum(((1-prob)^(j-1)-(1-prob)^(j))*prob_r*sum(bTimeI2[Ma.i,Ma.j,j,2,k] for k in (t - p.t+1):t if (t - p.t+1 >= 1)) for Ma in MIJ, p in IJT, j in Jop[Ma.i] if (p.i == Ma.i && p.j == Ma.j && Ma.m == mi))
-        <= MachineCap[mi]
-        )
+        <= MachineCap[mi] # TODO: maybe...
+    )
+
+    @constraints(m, begin
+        [i=I, j=Jop[i]], bTime1[i,j] - sbTime1[i,j] <= feasibility_window
+        [i=I, j=Jop[i]], bTime1[i,j] - sbTime1[i,j] >= -feasibility_window
+
+        [i=I, j=Jop[i]], sum(bTimeI1[i,j,t] for t in T, P in IJT if has_ij(P,i,j)) == 1
+        [i=I, j=Jop[i]], sum(t*bTimeI1[i,j,t] for t in T, P in IJT if has_ij(P,i,j)) == bTime1[i,j]
+        [i=I, j=Jop[i]], sum((t+P.t)*bTimeI1[i,j,t] for t in T, P in IJT if has_ij(P,i,j)) - 1 == cTime1[i,j]
+
+        [i=I, j=Jop[i], j1=Jop[i], r=R], sum(bTimeI2[i,j,j1,r,t] for t in T, P in IJT if has_ij(P,i,j)) == 1
+        [i=I, j=Jop[i], j1=Jop[i], r=R], sum(t*bTimeI2[i,j,j1,r,t] for t in T, P in IJT if has_ij(P,i,j)) == bTime2[i,j,j1,r]
+        [i=I, j=Jop[i], j1=Jop[i], r=R], sum((t+P.t)*bTimeI2[i,j,j1,r,t] for t in T, P in IJT if has_ij(P,i,j)) - 1  == cTime2[i,j,j1,r]
+
+        [i=I, j1=Jop[i], r=R],  ComTime2[i,j1,r] == cTime2[i,J[i],j1,r]
+        [i=I],                  ComTime1[i]      == cTime1[i,J[i]]
+    end)
 
     optimize!(m)
-
+    @show termination_status(m)
     jsp.status.time_solve_feasibility += solve_time(m)
     valid_flag = valid_solve(FeasibilityProblem(), m)
     if valid_flag
-        jsp.status.upper_bound[current_iteration] = objective_value(m)
+        @show objective_value(m)
+        jsp.status.upper_bound[jsp.status.current_iteration] = objective_value(m)
     end
 
     close_problem!(m)
