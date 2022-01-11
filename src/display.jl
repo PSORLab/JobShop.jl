2eedbeed231d301548d0e6c4a332800870c2e463
Matthew Wilhelm <matthew.wilhelm@uconn.edu>
2022-01-11 12:24:17 -0500

    Update quality metric

--- src/display.jl
+++ src/display.jl
@@ -7,7 +7,7 @@ function display_iteration(j::JobShopProblem, i::Int)
     @unpack maxest, estimate, penalty, current_iteration, time_start, time_solve_subprob, 
             time_solve_stepsize, current_norm, current_step = j.status
     total_time = time() - time_start
-    qual = current_abs_gap(j)/estimate*100
+    qual = (estimate-current_lower_bound(j))/estimate*100
     if (current_upper_bound(j) < 100000.0) || (j.parameter.verbosity > 1)
         outstring = @sprintf("Iteration:  %4i, (S):  %4i  ", current_iteration, i)
         outstring *= @sprintf("SDV:  %.3e  FC:  %.3e  ", current_lower_bound(j), current_upper_bound(j))
