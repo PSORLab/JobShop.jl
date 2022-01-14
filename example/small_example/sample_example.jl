82dfa8d926760e49401afe1b9643471dd9708a06
Matthew Wilhelm <matthew.wilhelm@uconn.edu>
2022-01-13 23:15:35 -0500

    Update examples

--- example/small_example/sample_example.jl
+++ example/small_example/sample_example.jl
@@ -13,17 +13,14 @@ jsprob.parameter.prob_r = 0.2
 
 # set hyperparameter
 jsprob.parameter.start_upper_bound = 8000.0
-jsprob.parameter.start_norm = 100.0
+jsprob.parameter.start_norm = 200.0
 jsprob.parameter.start_step = 0.006
 jsprob.parameter.optimizer = CPLEX.Optimizer
 jsprob.parameter.penalty = 60.0
 jsprob.parameter.alpha_step_1 = 0.5/20
 jsprob.parameter.alpha_step_2 = 0.5
-jsprob.parameter.feasible_interval = 21
-jsprob.parameter.feasible_norm_limit = 0.7 # TODO: CHANGE BACK TO 2
-jsprob.parameter.feasibility_window = 2
-jsprob.parameter.stepsize_interval = 40
-jsprob.parameter.stepsize_start = 37.5
+jsprob.parameter.feasible_norm_limit = 2.0 # TODO: CHANGE BACK TO 2
+jsprob.parameter.feasibility_window = 3
 jsprob.parameter.iteration_limit = 1000
 
 # solve
